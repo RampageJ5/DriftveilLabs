9163bfd679c2e1a40eb8346b042922635feb59d9
Jay Annadurai <ajayanth@outlook.com>
2024-05-19 00:43:48 -0400

    Introduce Root Index Page

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Author: Jay Annadurai
+ * Date: 18 May 2024
+ * Name: index.php
+ * Description: Root Webpage File that Relies on the Dispatcher Class to Redirect to the Appropriate Method
+ */
+
+//Use Composer's Autoload
+//To Refresh, Change Directory (CD) and then 'composer dumpautoload -o'
+require_once ("Application/Config.class.php");
+
+////Execute the Dispatcher's Dispatch Method
+////$dispatch = new Dispatcher();
+//DispatcherJ::debug();
+//DispatcherJ::dispatch();
+
